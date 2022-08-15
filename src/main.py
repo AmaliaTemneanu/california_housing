ca43f83048360ab1327b154c918160d41f9f9ce5
Amalia Temneanu <amalia.temneanu@gmail.com>
2022-08-15 17:40:11 +0200

    added .env

--- src/main.py
+++ src/main.py
@@ -54,13 +54,19 @@ from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 from sklearn.pipeline import Pipeline
 
+import os
+import dotenv
+
 
 # ### User Dependent Variables
 
 # In[10]:
 
+project_dir = os.path.join(os.path.dirname(__file__), os.pardir)
+dotenv_path = os.path.join(project_dir, '.env')
+dotenv.load_dotenv(dotenv_path)
 
-data_path = "../data/raw/california_housing_0.csv"
+data_path = os.getenv("DATA_PATH")
 
 
 # <a id='P1'></a>
